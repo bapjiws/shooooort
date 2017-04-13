8549df8081b196954993c7450fbc169844e52135
bapjiws <alex.boklin@gmail.com>
2017-04-13 19:39:31 +0300

    Add React Boostrap to the mix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 import '../styles/main';
 
+import { Grid, Row, Col } from 'react-bootstrap/lib';
+
 const App = () => {
-    return <div className="logo">Waddup, dawg?</div>
+    return <Grid>
+        <Row>
+            <Col xs={6} md={6}><div className="logo">Shooooort</div></Col>
+            <Col xs={6} md={6}><div className="logo">Shooooort</div></Col>
+        </Row>
+    </Grid>
 };
 
 export default App;
