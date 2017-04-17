e3667c1499135307733cef5dc79e9a28f57248e5
bapjiws <alex.boklin@gmail.com>
2017-04-17 17:35:56 +0300

    Test reducers for failures

--- tests/linksData.js
+++ tests/linksData.js
@@ -5,7 +5,9 @@ import linksDataReducer from '../redux/reducers/linksData';
 
 import {
     ADD_LINKS_DATA_ENTRY_SUCCESS,
+    ADD_LINKS_DATA_ENTRY_FAILURE,
     UPDATE_LINKS_DATA_SUCCESS,
+    UPDATE_LINKS_DATA_FAILURE,
     CLEAR_LINKS_DATA
 } from '../redux/actions/types';
 
@@ -39,6 +41,56 @@ test('should add a new entry to the data object with stats on links', t => {
     };
 
     t.deepEqual(linksDataReducer(deepFreeze(linksDataBefore), deepFreeze(action)), linksDataAfter);
+    t.end();
+});
+
+test('should add an error to the data object with stats on links, otherwise leave it intact (2 actions)', t => {
+    const linksDataBefore = {
+        data: {
+            '918dbe': {
+                url: 'http://example.com/1',
+                startDate: '2017-02-17T12:26:03.199Z',
+                lastSeenDate: '2017-02-17T12:26:03.199Z',
+                redirectCount: 0
+            },
+            'e67b': {
+                url: 'http://example.com/3',
+                startDate: '2017-04-17T11:56:08.830Z',
+                lastSeenDate: '2017-04-17T13:17:50.871Z',
+                redirectCount: 0
+            }
+        },
+        error: null
+    };
+    const action_one = {
+        type: ADD_LINKS_DATA_ENTRY_FAILURE,
+        error: 'A VERY DRAMATIC ERROR ;('
+    };
+    const action_two = {
+        type: UPDATE_LINKS_DATA_FAILURE,
+        error: 'A VERY DRAMATIC ERROR ;('
+    };
+
+    const linksDataAfter = {
+        data: {
+            '918dbe': {
+                url: 'http://example.com/1',
+                startDate: '2017-02-17T12:26:03.199Z',
+                lastSeenDate: '2017-02-17T12:26:03.199Z',
+                redirectCount: 0
+            },
+            'e67b': {
+                url: 'http://example.com/3',
+                startDate: '2017-04-17T11:56:08.830Z',
+                lastSeenDate: '2017-04-17T13:17:50.871Z',
+                redirectCount: 0
+            }
+        },
+        error: 'A VERY DRAMATIC ERROR ;('
+    };
+
+    t.deepEqual(linksDataReducer(deepFreeze(linksDataBefore), deepFreeze(action_one)), linksDataAfter);
+    t.deepEqual(linksDataReducer(deepFreeze(linksDataBefore), deepFreeze(action_two)), linksDataAfter);
     t.end();
 });
 
