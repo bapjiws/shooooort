5ec3801c3c723b6d85dd1ae5170f1d82b2a67da6
bapjiws <alex.boklin@gmail.com>
2017-04-17 21:50:44 +0300

    Remove unnecessary divs from Headline component

--- src/Headline.jsx
+++ src/Headline.jsx
@@ -4,8 +4,8 @@ import { Row, Col } from 'react-bootstrap/lib';
 
 const Headline = () => <Row className="headline">
     <Col xs={2} md={2}></Col>
-    <Col xs={4} md={4}><div className="text-logo">Shooooort</div></Col>
-    <Col xs={4} md={4}><div className="text-normal text-align-end">The link shortener with a long name</div></Col>
+    <Col className="text-logo" xs={4} md={4}>Shooooort</Col>
+    <Col className="text-normal text-align-end" xs={4} md={4}>The link shortener with a long name</Col>
     <Col xs={2} md={2}></Col>
 </Row>;
 
