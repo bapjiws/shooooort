55162305651e05ab312385646b667918f0ef7e6b
bapjiws <alex.boklin@gmail.com>
2017-05-08 19:31:34 +0300

    Change document's title and the headline

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap/lib';
 
 const Headline = () => <Row className="headline flex-cross-axis-align-baseline">
-    <Col className="text-logo" md={6}>Shooooort</Col>
-    <Col className="text-normal padding-left-description" md={6}>The link shortener with a long name</Col>
+    <Col className="text-logo" md={6}>URL Shortener</Col>
+    <Col className="text-normal padding-left-description" md={6}>Does exactly what it's supposed to.</Col>
 </Row>;
 
 export default Headline;
