5a565c9d61e6ca0422a0050840802fa5efb426e8
bapjiws <alex.boklin@gmail.com>
2017-05-14 23:40:53 +0300

    Enable console logging in production for debugging

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+if (process.env.NODE_ENV === 'production') console.log('HOST:', `${process.env.HEROKU_URL}:${process.env.PORT}`);
+// const dotEnvVars = require('dotenv').config().parsed;
+// console.log('dotEnvVars:', dotEnvVars);
+console.log('process.env:', process.env);
+
 export const getAxiosInstance = () => {
     return axios.create({
         // Our proxy server -- mimics all the routes exactly so that it can be substituted w/ a CORS-supporting server.
