9679984c34a1db375ef997b4534c886132c82335
bapjiws <alex.boklin@gmail.com>
2017-05-14 22:09:20 +0300

    Use process.env.PROXY in development again

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getAxiosInstance = () => {
     return axios.create({
         // Our proxy server -- mimics all the routes exactly so that it can be substituted w/ a CORS-supporting server.
-        baseURL: process.env.NODE_ENV === 'production' ? `${process.env.HEROKU_URL}:${process.env.PORT}` : 'https:localhost:8080',
+        baseURL: process.env.NODE_ENV === 'production' ? `${process.env.HEROKU_URL}:${process.env.PORT}` : process.env.PROXY,
         headers: {'Content-Type': 'application/json'}
     });
 };
