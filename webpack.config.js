8bf602e25e7bf32883d914d23d8924c67be1ff67
bapjiws <alex.boklin@gmail.com>
2017-05-15 13:49:12 +0300

    Use EnvironmentPlugin in production

--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,8 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const inProductionMode = NODE_ENV === 'production';
 
-let envVars = {};
-if (inProductionMode) {
-    // TODO: check out EnvironmentPlugin: https://webpack.js.org/plugins/environment-plugin/
-    envVars = {
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        // 'process.env.PORT': JSON.stringify(process.env.PORT),
-        'process.env.GOOGLE_URL_SHORTENER_API:': JSON.stringify(process.env.GOOGLE_URL_SHORTENER_API),
-        'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-        'process.env.HEROKU_URL': JSON.stringify(process.env.HEROKU_URL)
-    }
-} else {
-    const dotEnvVars = require('dotenv').config().parsed;
-    envVars = Object.keys(dotEnvVars).
-    reduce( (acc, key) => {
-        acc['process.env'][key] = JSON.stringify(dotEnvVars[key]);
-        return acc;
-    }, {
-        'process.env': {
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        }
-    });
-}
-
 const plugins = [
     new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.DefinePlugin(envVars),
     new HtmlWebpackPlugin({
         template: path.join(__dirname, '/src/index.html')
     }),
@@ -40,12 +16,25 @@ const plugins = [
 
 if (inProductionMode) {
     plugins.push(
+        new webpack.EnvironmentPlugin(['NODE_ENV', 'GOOGLE_URL_SHORTENER_API', 'API_KEY', 'HEROKU_URL']),
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest']
         })
     )
 } else {
+    const dotEnvVars = require('dotenv').config().parsed;
+    const envVars = Object.keys(dotEnvVars).
+    reduce( (acc, key) => {
+        acc['process.env'][key] = JSON.stringify(dotEnvVars[key]);
+        return acc;
+    }, {
+        'process.env': {
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        }
+    });
+
     plugins.push(
+        new webpack.DefinePlugin(envVars),
         new webpack.HotModuleReplacementPlugin() // <-- To generate hot update chunks
     )
 }
