e63471a8233328eaa22157dded8f10dae982b822
bapjiws <alex.boklin@gmail.com>
2017-06-04 23:01:35 +0300

    Move link list's rows into separate components

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import TimeAgo from 'react-timeago';
-import CopyToClipboard from 'react-copy-to-clipboard';
+import LinkListRow from './LinkListRow';
 
 import { fetchLinksInfo } from '../../redux/actions/linksData';
 
 class LinkList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            mouseOver: false,
-            clicked: false
-        };
-    }
-
     componentDidMount() {
         this.props.fetchLinksInfo();
         // this.timerID = setInterval(
@@ -30,7 +20,6 @@ class LinkList extends Component {
 
     render() {
         const { linksData } = this.props;
-        const { mouseOver, clicked } = this.state;
 
         return (
             <section>
@@ -47,37 +36,7 @@ class LinkList extends Component {
                 </div>
                 {
                     Object.keys(linksData).map(key =>
-                        <div className="link-list-row" key={key}>
-                            <div className="link-list-row-link">
-                                <CopyToClipboard text={`http://goo.gl/${key}`}>
-                                    <div>
-                                        <span
-                                            onMouseOver={() => this.setState({mouseOver: true})}
-                                            onMouseLeave={() => {
-                                                this.setState({mouseOver: false});
-                                                this.setState({clicked: false})
-                                            }}
-                                            onClick={() => this.setState({clicked: true})}
-                                            >
-                                            <span className="text-url-body">http://goo.gl/</span>
-                                            <span className="text-url-shortcode">{ `${key}` }</span>
-                                        </span>
-                                        <span
-                                            className={mouseOver && !clicked ?
-                                                'copy-suggestion-visible' : 'copy-suggestion-hidden'}>
-                                            Click to copy this link
-                                        </span>
-                                    </div>
-                                </ CopyToClipboard>
-                                <a className="text-url-original" href={ linksData[key].url } target="_blank">{ linksData[key].url }</a>
-                            </div>
-                            <div className="link-list-row-visits text-data text-align-center">
-                                { linksData[key].visits }
-                            </div>
-                            <div className="link-list-row-last-visited text-data text-align-center">
-                                { <TimeAgo date={linksData[key].lastVisited} live={false} /> }
-                            </div>
-                        </div>
+                        <LinkListRow key={key} rowId={key} linksData={linksData}/>
                     )
                 }
             </section>
---
+++ src/components/LinkListRow.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import TimeAgo from 'react-timeago';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+class LinkListRow extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mouseOver: false,
+            clicked: false
+        };
+    }
+
+    render() {
+        const { rowId, linksData } = this.props;
+        const { mouseOver, clicked } = this.state;
+
+        return (
+            <div className="link-list-row" key={rowId}>
+                <div className="link-list-row-link">
+                    <CopyToClipboard text={`http://goo.gl/${rowId}`}>
+                        <div>
+                            <span
+                                onMouseOver={() => this.setState({mouseOver: true})}
+                                onMouseLeave={() => {
+                                    this.setState({mouseOver: false});
+                                    this.setState({clicked: false})
+                                }}
+                                onClick={() => this.setState({clicked: true})}
+                            >
+                                <span className="text-url-body">http://goo.gl/</span>
+                                <span className="text-url-shortcode">{ `${rowId}` }</span>
+                            </span>
+                            <span
+                                className={mouseOver && !clicked ?
+                                    'copy-suggestion-visible' : 'copy-suggestion-hidden'}>
+                                            Click to copy this link
+                                        </span>
+                        </div>
+                    </ CopyToClipboard>
+                    <a className="text-url-original" href={ linksData[rowId].url } target="_blank">{ linksData[rowId].url }</a>
+                </div>
+                <div className="link-list-row-visits text-data text-align-center">
+                    { linksData[rowId].visits }
+                </div>
+                <div className="link-list-row-last-visited text-data text-align-center">
+                    { <TimeAgo date={linksData[rowId].lastVisited} live={false} /> }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default LinkListRow;
