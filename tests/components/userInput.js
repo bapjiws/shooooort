daf8eeb5eae305ea9fa3be8622414c14fa5a63c8
bapjiws <alex.boklin@gmail.com>
2017-06-11 13:01:22 +0300

    Simulate input's change rather than change component's state

--- tests/components/userInput.js
+++ tests/components/userInput.js
@@ -15,18 +15,38 @@ test.beforeEach(t => {
 test('UserInput w/ invalid input should show feedback, cross icon and have "incorrect" style on focus', t => {
     const { wrapper, inputHelp } = t.context.data;
 
-    wrapper.setState({ input: 'url' });
-    t.is(wrapper.find('input[type="text"]').hasClass('text-input input-focus-incorrect'), true); // or simply input-focus-incorrect
-    t.is(wrapper.contains(inputHelp), true);
-    t.is(wrapper.find('svg').hasClass('incorrect-input-icon'), true);
+    t.is(wrapper.find('input[type="text"]').hasClass('text-input'), true);
 
+    'http:/'.split('').reduce((acc, curr) => {
+        acc += curr;
+        wrapper.find('input[type="text"]').simulate('change', { target: { value: acc } });
+
+        t.is(wrapper.state('input'), acc);
+        t.is(wrapper.find('input[type="text"]').hasClass('text-input input-focus-incorrect'), true);
+        t.is(wrapper.contains(inputHelp), true);
+        t.is(wrapper.find('svg').hasClass('incorrect-input-icon'), true);
+        return acc;
+    }, '');
 });
 
 test('UserInput w/ valid input should not show feedback, contain check icon and have "correct" style on focus', t => {
     const { wrapper, inputHelp } = t.context.data;
 
-    wrapper.setState({ input: 'http://', inputIsValid: true });
+    wrapper.find('input[type="text"]').simulate('change', { target: { value: 'http://' } });
+
     t.is(wrapper.find('input[type="text"]').hasClass('text-input input-focus-correct'), true); // or simply input-focus-correct
     t.is(wrapper.contains(inputHelp), false);
     t.is(wrapper.find('svg').hasClass('correct-input-icon'), true);
+});
+
+test.skip('UserInput should dispatch action, blur on Enter w/ valid input and clear the input field', t => {
+    const { wrapper } = t.context.data;
+
+    wrapper.find('input[type="text"]').simulate('change', { target: { value: 'http://' } });
+    t.is(wrapper.state('input'), 'http://');
+    wrapper.find('input[type="text"]').simulate('keyDown', {key: 'Enter', keyCode: 13});
+    t.is(wrapper.state('input'), '');
+
+    // TODO: props.shortenLink should be called -- create spy and pass mock store or wrap into Provider
+    // TODO: check if text input no longer has focus
 });
