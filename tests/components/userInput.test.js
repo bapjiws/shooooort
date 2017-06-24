6402ebaf6debff5a042aa2c7e526d02bb2e322e7
bapjiws <alex.boklin@gmail.com>
2017-06-24 13:23:02 +0300

    Use Jest's spies instead of Sinon's ones

--- tests/components/userInput.test.js
+++ tests/components/userInput.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { spy } from 'sinon'; // TODO: use jest.spyOn(object, methodName)
 
 // Import the unconnected version -- see http://redux.js.org/docs/recipes/WritingTests.html#connected-components
 import { UserInput } from '../../src/components/UserInput';
@@ -47,38 +46,30 @@ test('UserInput should blur, fire shortenLink and clear the input field on Enter
     // and https://github.com/airbnb/enzyme/issues/566. We will mount the component into the document and detect
     // changes in document.activeElement.
     document.body.innerHTML = '<div id="root"></div>';
-    const shortenLink = spy();
+    const shortenLink = jest.fn();
     const wrapper = mount(<UserInput shortenLink={shortenLink}/>, { attachTo: document.body.firstChild });
 
     // Put the cursor into the input field. The first three lines are there for the sake of exercise, because the same
     // won't work with blur for some reason (commented out below). We'll be relying on document.activeElement.
-    spy(wrapper.find('input[type="text"]').getDOMNode(), 'focus');
+    const focusSpy = jest.spyOn(wrapper.find('input[type="text"]').getDOMNode(), 'focus');
     wrapper.find('input[type="text"]').getDOMNode().focus();
-    expect(wrapper.find('input[type="text"]').getDOMNode().focus.calledOnce).toBe(true);
-    // OR:
-    // spy(document.querySelector('input[type="text"]'), 'focus')
-    // document.querySelector('input[type="text"]').focus();
-    // t.is(document.querySelector('input[type="text"]').focus.calledOnce, true);
+    expect(focusSpy).toHaveBeenCalled();
     expect(document.activeElement.nodeName).toBe('INPUT');
 
     // Type correct input and hit Enter
-    spy(wrapper.find('input[type="text"]').getDOMNode(), 'blur');
-    // OR:
-    // spy(document.querySelector('input[type="text"]'), 'blur');
-
+    const blurSpy = jest.spyOn(wrapper.find('input[type="text"]').getDOMNode(), 'blur');
     wrapper.find('input[type="text"]').simulate('change', { target: { value: 'http://' } });
     expect(wrapper.state('input')).toBe('http://');
     wrapper.find('input[type="text"]').simulate('keyDown', {key: 'Enter', keyCode: 13});
     expect(wrapper.state('input')).toBe('');
-    expect(shortenLink.calledOnce).toBe(true); // shortenLink is tested separately in actions
-    expect(wrapper.find('input[type="text"]').getDOMNode().blur.calledOnce).toBe(true);
-    // OR:
-    // t.is(document.querySelector('input[type="text"]').blur.calledOnce, true);
+    expect(shortenLink).toHaveBeenCalled(); // shortenLink is tested separately in actions
+    expect(blurSpy).toHaveBeenCalled();
     expect(document.activeElement.nodeName).toBe('BODY');
 
+
     // Trying to test form submission (needed to test if event.preventDefault() has been called on Enter). Won't work:
-    // spy(wrapper.find('form').getDOMNode(), 'submit');
+    // const submitSpy = jest.spyOn(wrapper.find('form').getDOMNode(), 'submit');
     // wrapper.find('input[type="text"]').simulate('change', { target: { value: 'http://' } });
-    // wrapper.find('input[type="text"]').simulate('keyDown', {key: 'Enter', keyCode: 13});
-    // t.is(wrapper.find('form').getDOMNode().submit.calledOnce, true);
+    // wrapper.find('input[type="text"]').simulate('keyDown', { key: 'Enter', keyCode: 13 });
+    // expect(submitSpy).toHaveBeenCalled();
 });
