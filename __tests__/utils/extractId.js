1067a5d92a01a4b289563edc062a834e52ac1a96
bapjiws <alex.boklin@gmail.com>
2017-07-09 21:35:44 +0300

    Generative tests for responseDataIdToId

--- __tests__/utils/extractId.js
+++ __tests__/utils/extractId.js
@@ -1,11 +1,17 @@
 import { responseDataIdToId } from '../../utils/extractId';
+// See https://github.com/leebyron/testcheck-js/tree/master/integrations/jasmine-check
+require('jasmine-check').install();
 
 describe('responseDataIdToId', () => {
-    it('should correctly extract id from URLs beginning with http', () => {
-        expect(responseDataIdToId('http://goo.gl/s0m31d')).toBe('s0m31d');
-    });
+    check.it(
+        'should correctly extract id from URLs beginning with http',
+        gen.asciiString.notEmpty(),
+        code => expect(responseDataIdToId(`https://goo.gl/${code}`)).toBe(code)
+    );
 
-    it('should correctly extract id from URLs beginning with https', () => {
-        expect(responseDataIdToId('https://goo.gl/an0th3r1d')).toBe('an0th3r1d');
-    });
+    check.it(
+        'should correctly extract id from URLs beginning with https',
+        gen.asciiString.notEmpty(),
+        code => expect(responseDataIdToId(`https://goo.gl/${code}`)).toBe(code)
+    );
 });
